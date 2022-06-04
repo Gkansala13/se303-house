34206e29c5eb6597db442f9481fc0e2dd9d73a09
gkansala13 <kansalag@oregonstate.edu>
2022-06-04 14:05:07 -0700

    Removed case statment from line method, single line of method now
    Reference #4

--- lib/house.rb
+++ lib/house.rb
@@ -24,38 +24,13 @@ class House
       "the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
     when 11
       "the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
-    when 12
+    else
       "the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
     end
   end
 
   def line(number)
-    case number
-    when 1
-      "This is #{lyrics(number)}the house that Jack built.\n"
-    when 2
-      "This is #{lyrics(number)}the house that Jack built.\n"
-    when 3
-      "This is #{lyrics(number)}the house that Jack built.\n"
-    when 4
-      "This is #{lyrics(number)}the house that Jack built.\n"
-    when 5
-      "This is #{lyrics(number)}the house that Jack built.\n"
-    when 6
-      "This is #{lyrics(number)}the house that Jack built.\n"
-    when 7
-      "This is #{lyrics(number)}the house that Jack built.\n"
-    when 8
-      "This is #{lyrics(number)}the house that Jack built.\n"
-    when 9
-      "This is #{lyrics(number)}the house that Jack built.\n"
-    when 10
-      "This is #{lyrics(number)}the house that Jack built.\n"
-    when 11
-      "This is #{lyrics(number)}the house that Jack built.\n"
-    else
-      "This is #{lyrics(number)}the house that Jack built.\n"
-    end
+    "This is #{lyrics(number)}the house that Jack built.\n"
   end
 
   def recite 
