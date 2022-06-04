3bc3dbc1d1c17d3a2f05784bc69ffcc9659021df
gkansala13 <kansalag@oregonstate.edu>
2022-06-04 16:59:38 -0700

    Called SONG_PHRASE in lyrics method in case 3
    Reference #5

--- lib/house.rb
+++ lib/house.rb
@@ -7,7 +7,7 @@ class House
     when 2
       SONG_PHRASES[number-1] + lyrics(number-1)
     when 3
-      "the rat that ate #{lyrics(number-1)}"
+      SONG_PHRASES[number-1] + lyrics(number-1)
     when 4
       "the cat that killed #{lyrics(number-1)}"
     when 5 
